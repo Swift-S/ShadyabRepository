bc164e543cecd642b6fe7ddb9b9eb7fe368e2f54
Amir Daliri <amir.dalirii69@yahoo.com>
2016-12-04 11:11:16 +0330

    Initial Commit

---
+++ soheil-intro1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soheil-intro1
+//
+//  Created by Amir Daliri on 12/4/16.
+//  Copyright © 2016 Amir Daliri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
