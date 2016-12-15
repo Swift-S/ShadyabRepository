f46ca2c368c514b89ea3320c8835a636f667271b
Amir Daliri <amir.dalirii69@yahoo.com>
2016-12-16 02:43:59 +0330

    16Dec_worked_4h

--- IntroMenue.swift
+++ IntroMenue.swift
@@ -12,23 +12,35 @@ import Foundation
 class IntroMenue: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    var MenuIcon = [String]()
+    var ImgIcon = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        var MenuIcon = [UIImage(contentsOfFile: "ic_best_chef.png"),UIImage(contentsOfFile: "ic_best_chef.png")]
+        MenuIcon = ["دسته بندی","sdfsdf","sdfsdfsd"]
+        ImgIcon = ["ic_best_chef.png","ic_best_chef.png","ic_best_chef.png"]
+//        ImgIcon = [UIImage(contentsOfFile: "ic_best_chef.png")!,UIImage(contentsOfFile: "ic_best_chef.png")!]
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return MenuIcon.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "kjdnflwndc"
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")! as UITableViewCell
+        cell.textLabel?.text = MenuIcon[indexPath.row]
+        cell.textLabel?.font = UIFont(name: "DroidNaskh.ttf", size: 20)
+        cell.textLabel?.textColor = UIColor.blueColor()
+        var iconName = UIImage(named: MenuIcon[indexPath.row])
+        cell.imageView?.image = UIImage(named: self.ImgIcon[indexPath.row])
         return cell
     }
     
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
